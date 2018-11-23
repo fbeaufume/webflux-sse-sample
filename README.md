de933c12bbb6aa370cb663a6d47396922bd55424
Florian Beaufumé <fbeaufume@gmail.com>
2018-11-23 18:39:12 +0100

    Added an empty Spring Boot application

---
+++ README.md
@@ -0,0 +1,7 @@
+# WebFlux SSE Sample
+
+This is a sample Spring Boot reactive application that uses SSE (Server-Sent Events).
+
+Run with `mvn spring-boot:run`.
+
+Then connect to <http://localhost:8080/>.
---
+++ src/main/java/com/adeliosys/sample/ReactiveSseApplication.java
@@ -0,0 +1,12 @@
+package com.adeliosys.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveSseApplication.class, args);
+    }
+}
